fbce16b3bd36a878660060b9ffe03f2eb73f9c5f
Huillca Reynaldo <lymreynaldo@icloud.com>
2024-07-15 06:04:48 -0300

    chore: initial commit

---
+++ src/main/java/uk/layme/s3_download_api/S3DownloadApiApplication.java
@@ -0,0 +1,13 @@
+package uk.layme.s3_download_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S3DownloadApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S3DownloadApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/uk/layme/s3_download_api/S3DownloadApiApplicationTests.java
@@ -0,0 +1,13 @@
+package uk.layme.s3_download_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S3DownloadApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
